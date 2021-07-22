353557d49dad5d7b013140510e22fd20f4dae249
Maxim Karasev <begs@disroot.org>
2021-07-22 21:33:35 +0300

    config: nvim: add more fields to lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -179,7 +179,7 @@ require'lualine'.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff'},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path = 1}, {'diagnostics', sources = nvim_lsp}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
