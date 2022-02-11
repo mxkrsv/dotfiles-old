42e797f1037be7dd39375f6a67dc4ca543658329
Maxim Karasev <begs@disroot.org>
2022-02-12 00:57:45 +0300

    config: nvim: lualine: empty separators

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,8 +157,8 @@ require'lualine'.setup {
   options = {
     icons_enabled = false,
     theme = 'gruvbox',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
   },
