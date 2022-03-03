09b88b2538e54e3a15e6d0dc041863af5be352cf
Maxim Karasev <begs@disroot.org>
2022-03-04 00:45:44 +0300

    config: nvim: add pylsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -141,7 +141,7 @@ local on_attach = function(client, bufnr)
 end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "clangd", "gopls" }
+local servers = { "clangd", "gopls", "pylsp" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
