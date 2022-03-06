68e02af7824c34c76336f5a623b1472b7feddae1
Maxim Karasev <begs@disroot.org>
2022-03-07 00:09:33 +0300

    config: nvim: drop compe

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,7 +31,6 @@ require('packer').startup(function()
     run = ':TSUpdate'
   }
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
   use {
     'hoob3rt/lualine.nvim',
     --requires = {'kyazdani42/nvim-web-devicons', opt = true}
@@ -47,47 +46,6 @@ g.gruvbox_invert_selection = false
 g.gruvbox_contrast_dark = 'medium'
 execute 'colorscheme gruvbox'
 execute 'hi Normal guibg=none'
-
--- Setup completion
-o.completeopt = "menuone,noselect"
-map('i', '<silent><expr> <C-Space>', "compe#complete()")
-map('i', '<silent><expr> <CR>', "compe#confirm('<CR>')")
-map('i', '<silent><expr> <C-e>', "compe#close('<C-e>')")
-map('i', '<silent><expr> <C-f>', "compe#scroll({ 'delta': +4 })")
-map('i', '<silent><expr> <C-d>', "compe#scroll({ 'delta': -4 })")
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  resolve_timeout = 800;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = {
-    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-    max_width = 120,
-    min_width = 60,
-    max_height = math.floor(vim.o.lines * 0.3),
-    min_height = 1,
-  };
-
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    vsnip = true;
-    ultisnips = true;
-    luasnip = true;
-  };
-}
 
 -- Setup treesitter
 require'nvim-treesitter.configs'.setup {
