babda598942926d7699d2f313218d9034f00de56
Maxim Karasev <begs@disroot.org>
2022-03-25 02:51:01 +0300

    config: nvim: use nvim-cmp as autocompletion engine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,6 +38,102 @@ o.foldmethod = 'expr'
 o.foldexpr = 'nvim_treesitter#foldexpr()'
 o.foldnestmax = 1
 
+-- Setup autocompletion
+o.completeopt = 'menu,menuone,noselect'
+local cmp = require'cmp'
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line,
+    true)[1]:sub(col, col):match('%s') == nil
+end
+cmp.setup({
+  --snippet = {
+  --  -- REQUIRED - you must specify a snippet engine
+  --  expand = function(args)
+  --    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+  --    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+  --    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+  --    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+  --  end,
+  --},
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+
+    ['<Tab>'] = function(fallback)
+      if not cmp.select_next_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+
+    ['<S-Tab>'] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+
+    -- Specify `cmp.config.disable` if you want to remove the default `<C-y>`
+    -- mapping.
+    ['<C-y>'] = cmp.config.disable,
+
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+
+    -- Accept currently selected item. Set `select` to `false` to only confirm
+    -- explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    -- You can specify the `cmp_git` source if you were installed it.
+    { name = 'cmp_git' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work
+-- anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't
+-- work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
 -- Setup LSP
 local lspconfig = require 'lspconfig'
 -- Mappings.
@@ -80,7 +176,8 @@ for _, lsp in pairs(servers) do
     flags = {
       -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
-    }
+    },
+    capabilities = capabilities
   }
 end
 
