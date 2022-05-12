fecff3d58acf5a9b6f9d432195e8700b6c247b81
Maxim Karasev <begs@disroot.org>
2022-05-13 00:20:33 +0300

    config: nvim: lualine: enable icons selectively

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -187,9 +187,11 @@ local lualine_options = {
   theme = 'gruvbox',
   disabled_filetypes = {},
   always_divide_middle = true,
+  icons_enabled = false
 }
+local lualine_icons = true
 if os.getenv("TERM") == "linux" then
-  lualine_options.icons_enabled = false
+  lualine_icons = false
   lualine_options.component_separators = { left = '', right = '' }
   lualine_options.section_separators = { left = '', right = '' }
 end -- disable powerline if on tty
@@ -197,10 +199,10 @@ require'lualine'.setup {
   options = lualine_options,
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff',
-                  {'diagnostics', sources={'nvim_diagnostic', 'coc'}}},
+    lualine_b = {{'branch', icons_enabled = lualine_icons}, 'diff',
+      'diagnostics'},
     lualine_c = {{'filename', path = 1, shorting_target = 0}},
-    lualine_x = {'encoding', {'fileformat', icons_enabled = false}, 'filetype'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
