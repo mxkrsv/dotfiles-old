f9c8a7ab55ea9d374dccb395335f40600c21e494
Maxim Karasev <begs@disroot.org>
2022-06-15 21:41:11 +0300

    config: nvim: cmp: fix cmdline and clean up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,15 +47,6 @@ local has_words_before = function()
     true)[1]:sub(col, col):match('%s') == nil
 end
 cmp.setup({
-  --snippet = {
-  --  -- REQUIRED - you must specify a snippet engine
-  --  expand = function(args)
-  --    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-  --    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-  --    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-  --    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-  --  end,
-  --},
   mapping = {
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -81,44 +72,26 @@ cmp.setup({
       end
     end,
 
-    -- Specify `cmp.config.disable` if you want to remove the default `<C-y>`
-    -- mapping.
-    ['<C-y>'] = cmp.config.disable,
-
     ['<C-e>'] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
 
-    -- Accept currently selected item. Set `select` to `false` to only confirm
-    -- explicitly selected items.
+    -- Set `select` to `false` to only confirm explicitly selected items.
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
   }, {
     { name = 'buffer' },
     { name = 'path' },
   })
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    -- You can specify the `cmp_git` source if you were installed it.
-    { name = 'cmp_git' },
-  }, {
-    { name = 'buffer' },
-  })
-})
-
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work
 -- anymore).
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
   }
@@ -127,6 +100,7 @@ cmp.setup.cmdline('/', {
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't
 -- work anymore).
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
