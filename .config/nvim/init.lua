83001865b0b0965fa8ffea1ced2ffb412e95e03d
Maxim Karasev <mxkrsv@disroot.org>
2022-12-18 20:21:12 +0300

    config: nvim: update and split gruvbox.nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,3 @@
-local execute = vim.api.nvim_command
-
 vim.highlight.on_yank = true
 vim.o.mouse = 'a'
 vim.o.number = true
@@ -9,15 +7,7 @@ vim.o.cursorline = true
 vim.o.cursorcolumn = true
 vim.o.title = true
 vim.o.scrolloff = 3
-
--- Setup theme
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.g.gruvbox_bold = true
-vim.g.gruvbox_italic = false
-vim.g.gruvbox_invert_selection = false
-vim.g.gruvbox_contrast_dark = 'medium'
-execute 'colorscheme gruvbox'
-execute 'hi Normal guibg=none'
 
 -- vim:et ts=2 sw=2
---
+++ .config/nvim/plugin/gruvbox.lua
@@ -0,0 +1,7 @@
+require("gruvbox").setup({
+	transparent_mode = true,
+})
+
+vim.cmd("colorscheme gruvbox")
+
+-- vim:et ts=2 sw=2
