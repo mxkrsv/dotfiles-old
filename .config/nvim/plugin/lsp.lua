3c4768822d3bd190437645e3aa6d62f813316fe4
Maxim Karasev <mxkrsv@disroot.org>
2023-01-06 02:55:35 +0300

    config: nvim: lsp: enable rust_analyzer

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -40,7 +40,7 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'clangd', 'gopls', 'pylsp' }
+local servers = { 'clangd', 'gopls', 'pylsp', 'rust_analyzer' }
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
